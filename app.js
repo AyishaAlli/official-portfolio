88f1d530fdf91dd366a9be8784e49cfbd07997c3
Ayisha Alli <alli.ayisha1@gmail.com>
2021-10-28 15:19:20 +0100

    added js files and created some variables

---
+++ app.js
@@ -0,0 +1,5 @@
+// Variables 
+
+const HamburgerMenu =  document.querySelector('.hamburger');
+const navList = document.querySelector('.navList');
+const exitBtn = document.querySelector('.exitBtn')
