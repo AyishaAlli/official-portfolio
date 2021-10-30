7c5047a2f3058c9057527426fee7dff83aa0d009
Ayisha Alli <alli.ayisha1@gmail.com>
2021-10-30 18:57:40 +0100

    made navbar fully responsive

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
-// Variables 
+/* VANILLA JAVASCRIPT WAY
 
 const HamburgerMenu =  document.querySelector('.hamburger');
 const navList = document.querySelector('.navList');
-const exitBtn = document.querySelector('.exitBtn')
+const exitBtn = document.querySelector('.close')
 
+HamburgerMenu.addEventListener('click', function toggleMenu(){
+    navList.classList.add('active');
+    navList.classList.remove('reverse_anim');
+    exitBtn.classList.add('show');
+})
+exitBtn.addEventListener('click', function toggleMenu(){
+    navList.classList.add('reverse_anim');
+    navList.classList.remove('active');
+    exitBtn.classList.remove('show');
+})
+*/
+
+// JQUERY
+
+$('.hamburger').on('click', () => {
+$('.navList').addClass('active').removeClass('reverse_anim')
+$('.close').addClass('show')
+});
+$('.close').on('click', () => {
+$('.navList').removeClass('active').addClass('reverse_anim')
+$('.close').removeClass('show')
+});
