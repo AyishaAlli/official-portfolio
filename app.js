c11e531aa2525e1963ae48dea490ec8b89a0e0ae
Ayisha Alli <alli.ayisha1@gmail.com>
2021-10-30 19:45:08 +0100

    removed dropdown menu when a link is clicked on

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+/*
 //VANILLA JAVASCRIPT WAY
 
 const HamburgerMenu =  document.querySelector('.hamburger');
 const navList = document.querySelector('.navList');
-const exitBtn = document.querySelector('.close')
+const exitBtn = document.querySelector('.close');
+const projectsBtn = document.querySelector('.projectBtn');
+const contactBtn = document.querySelector('.contactBtn');
 
 HamburgerMenu.addEventListener('click', function toggleMenu(){
     navList.classList.add('active');
@@ -10,20 +13,36 @@ HamburgerMenu.addEventListener('click', function toggleMenu(){
     exitBtn.classList.add('show');
 })
 exitBtn.addEventListener('click', function toggleMenu(){
-    navList.classList.add('reverse_anim');
     navList.classList.remove('active');
     exitBtn.classList.remove('show');
 })
 
+projectsBtn.addEventListener('click', function(){
+    navList.classList.remove('active');
+})
+contactBtn.addEventListener('click', function(){
+    navList.classList.remove('active');
+})
+*/
 
 // JQUERY
-/*
+
 $('.hamburger').on('click', () => {
 $('.navList').addClass('active').removeClass('reverse_anim')
 $('.close').addClass('show')
 });
+
 $('.close').on('click', () => {
 $('.navList').removeClass('active')
 $('.close').removeClass('show')
 });
-*/
+
+$('.projectBtn').on('click', () => {
+    $('.navList').removeClass('active')
+    $('.close').removeClass('show')
+})
+$('.contactBtn').on('click', () => {
+    $('.navList').removeClass('active')
+    $('.close').removeClass('show')
+})
+
